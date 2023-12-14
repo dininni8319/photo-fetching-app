73431d7e23b5777eeca0aca931136872a008a8d2
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-12-14 16:56:11 +0100

    completed the fetch photo application
    I created an helper function getPhoto that calls the fetchPhotoService. Also, I added a default photo to show it when there are not photo available. Additionally, I give some simple style to the application.

--- src/app/display-photo/display-photo.component.ts
+++ src/app/display-photo/display-photo.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchPhotoService } from '../fetch-photo.service';
 
 @Component({
   selector: 'app-display-photo',
   templateUrl: './display-photo.component.html',
   styleUrls: ['./display-photo.component.css']
 })
-export class DisplayPhotoComponent {
 
+export class DisplayPhotoComponent implements OnInit {
+  randomPhoto = ''
+  defaultPhoto = '../../assets/default-avatar.jpg'
+  
+  constructor(
+    private fetchPhotoService: FetchPhotoService
+  ) {
+    this.getPhoto()
+  }
+
+  onClick() {
+    this.getPhoto()
+  }
+
+  getPhoto() { 
+    this.fetchPhotoService.getRandomPhoto()
+      .subscribe((photo) => {
+        console.log('data: ',photo)
+        this.randomPhoto = photo.urls.small;
+    })
+  }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/fetch-photo.service.ts
+++ src/app/fetch-photo.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 
+interface Photo {
+  urls: {
+    small: string
+  }
+}
 @Injectable({
   providedIn: 'root'
 })
-export class FetchPhotoService {
 
-  constructor() { }
+
+export class FetchPhotoService {
+  accessKey = 'NSvWY5BdXylSU4aoukdrcTaMXtMl7gHTfz14hKotLUQ'
+  secretKey = 'Leq8JY0B2t-xDz8x_nPC3fh_RZRaNIQK_oiwHe5aH00'
+  // GET /photos/random
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getRandomPhoto():Observable<Photo> {
+    const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${this.accessKey}`
+    return this.http.get<Photo>(
+      apiUrl,
+      {
+        headers: {
+          Authorization: `Client-ID ${this.accessKey}`
+        } 
+      }
+    )
+      .pipe(map((data: any) => {
+        return { 
+          urls: {
+            small: data.urls.small
+          }
+        }
+      }))
+  }
 }
