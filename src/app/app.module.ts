c42bc246fe6eb4236d2f10c475245688d2b3a192
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-12-14 15:18:40 +0100

    created service and component for the photo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayPhotoComponent } from './display-photo/display-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayPhotoComponent
   ],
   imports: [
     BrowserModule,
